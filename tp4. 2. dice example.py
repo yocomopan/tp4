7b3306d769bb2dab681a66ab37de277137f9c58e
zheryakovi <zheryakovi@educ.college-montreal.qc.ca>
2025-11-11 10:44:16 -0500

    Added example dice

---
+++ tp4. 2. dice example.py
@@ -0,0 +1,15 @@
+"""
+2025
+by amna.siddiqui008
+Dice
+"""
+import random
+class Die(object):
+    def __init__(self, sides=6):
+        self.sides = sides
+
+    def roll(self):
+        return self.roll_count = random_randint(1,self.sides)
+
+
+
