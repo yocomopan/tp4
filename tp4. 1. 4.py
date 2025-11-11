83ec5a069467b3062357cc47c6616d72f96fd4f6
zheryakovi <zheryakovi@educ.college-montreal.qc.ca>
2025-11-11 10:26:21 -0500

    Modif. translated to french

--- tp4. 1. 4.py
+++ tp4. 1. 4.py
@@ -5,6 +5,7 @@ Exercice de Classe 1 #4
 """
 import random
 
+
 class Hero:
     def __init__(self, name):
         self.hp = random.randint(2, 10)
@@ -15,10 +16,10 @@ class Hero:
     def Damage_taken(self):
         if self.defense > self.attack:
             self.hp -= 0
-            print(f"{self.name} has {self.hp} hp after taking 0 dmg")
+            print(f"{self.name} a {self.hp} hp après avoir reçu 0 dégats")
         else:
             self.hp -= self.attack - self.defense
-            print(f"{self.name} has {self.hp} hp after taking {self.attack - self.defense} dmg")
+            print(f"{self.name} a {self.hp} hp après avoir reçu {self.attack - self.defense} dégats")
 
     def Alive(self):
         return self.hp > 0
@@ -28,4 +29,4 @@ h = Hero("Jack")
 
 h.Damage_taken()
 if not h.Alive():
-    print("They have Died")
+    print("Il/Elle est mort")
